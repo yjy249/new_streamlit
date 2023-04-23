8baa60be25941833b61b8764db2561ec546f8ce4
yjy249 <110330834+yjy249@users.noreply.github.com>
2023-04-23 19:54:17 +0800

    Update main.py

--- garbage_streamlit_new/main.py
+++ garbage_streamlit_new/main.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 #     st.text('This is a platform that can detect trash on highways.')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='garbage_streamlit_new/garm/weight/best.pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='garbage_streamlit_new/garm/weights/best.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default='garbage_streamlit_new/garm/data/images', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
